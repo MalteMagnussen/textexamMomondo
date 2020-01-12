2f9fd7fe16621d8fb8207167770d70bb06878b2f
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-01-12 18:51:00 +0100

    Working on AdminPage

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,6 +1,23 @@
 import React, { useState } from "react";
 import facade from "../apiFacade.jsx";
 
+const acceptable_categories = [
+  "career",
+  "celebrity",
+  "dev",
+  "explicit",
+  "fashion",
+  "food",
+  "history",
+  "money",
+  "movie",
+  "music",
+  "political",
+  "science",
+  "sport",
+  "travel"
+];
+
 const AdminPanel = ({ loggedIn, roles }) => {
   if (!loggedIn || !roles.includes("admin")) {
     return (
@@ -9,8 +26,16 @@ const AdminPanel = ({ loggedIn, roles }) => {
       </>
     );
   } else {
-    return <p>You are logged in as admin</p>;
+    return (
+      <>
+        <AdminPage />
+      </>
+    );
   }
+};
+
+const AdminPage = () => {
+  return <> Youre logged in as Admin. </>;
 };
 
 export default AdminPanel;
